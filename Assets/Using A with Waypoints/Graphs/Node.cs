2524d10f8500d33b4f30709053108cb8a58251e9
SaberGt-afk <gtsaber0000@gmail.com>
2024-10-15 10:27:00 +0100

    A Simple Graph API

---
+++ Assets/Using A with Waypoints/Graphs/Node.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node 
+{
+    public List<Edge> edgeList = new List<Edge>();
+    public Node path = null;
+    GameObject Id;
+    public float f, g, h;
+    public Node cameFrom;
+
+    private GameObject id;
+
+    public Node(GameObject i) 
+    {
+
+        id = i;
+
+        path = null;
+    }
+
+    public GameObject getID() 
+    {
+
+        return id;
+    }
+}
